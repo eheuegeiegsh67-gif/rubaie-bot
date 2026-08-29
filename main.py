1dcca57df3529aebc178a77df6e18012b5a8e6ba
eheuegeiegsh67-gif <eheuegeiegsh67@gmail.com>
2026-08-29 16:59:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import SessionPasswordNeeded
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-BOT_TOKEN = "8938082118:AAHWa070-4EKOTg5ukM50smhfyf"
+BOT_TOKEN = "8648735860:AAHIXbAq8by99wYWb1ZpkkWtDguX-lghBkA"
 API_ID = 38183563
 API_HASH = "7d3d3b379c6840a72c983865c8d927d"
 
